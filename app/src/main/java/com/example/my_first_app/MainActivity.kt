5fae72be57f1a8b1db914e64edc0b83b70e3509d
Minimalist30 <minimalist0030@gmail.com>
2025-12-15 23:51:24 +0900

    add text-field module

--- app/src/main/java/com/example/my_first_app/MainActivity.kt
+++ app/src/main/java/com/example/my_first_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,23 +14,46 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.my_first_app.ui.theme.MyfirstappTheme
 
+//追加
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyfirstappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GoodThingInputScreen()
             }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun GoodThingInputScreen() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("今日のよかったことを教えて")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("おっけー！")
+        }
+    }
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
